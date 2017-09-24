bab6b2f37011b757fba9aa31d1516374f5c32ae0
angelpjn <angelpjn@naver.com>
2017-09-24 23:02:49 +0900

    'ch.3'

---
+++ ch.3/src/main/java/ex/No9.java
@@ -0,0 +1,19 @@
+
+package ex;
+
+public class No9{
+    public static void main(String[] args){
+
+        int i;
+        int sum = 0;
+
+        for(i = 1;i <= 100; i++){
+        
+        sum += i;
+
+        }
+
+        System.out.println(sum);
+
+    }
+}
